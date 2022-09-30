09ebfe05090fa089c608b53a42284538bace9403
STKhieu12y8 <doanhuyxh@gmail.com>
2022-09-30 12:19:18 +0700

    submib_3

--- motel_managment.sql
+++ motel_managment.sql
@@ -23,16 +23,16 @@ DROP TABLE IF EXISTS `people`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `people` (
-  `admin_id` varchar(45) NOT NULL,
-  `admin_name` varchar(255) NOT NULL,
-  `admin_address` varchar(255) NOT NULL,
-  `admin_phone_number` varchar(45) NOT NULL,
+  `person_id` varchar(45) NOT NULL,
+  `person_name` varchar(255) NOT NULL,
+  `person_address` varchar(255) NOT NULL,
+  `person_phone_number` varchar(45) NOT NULL,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `describe` varchar(255) NOT NULL,
-  `rom_id` varchar(45) NOT NULL,
-  PRIMARY KEY (`admin_id`),
-  UNIQUE KEY `admin_id_UNIQUE` (`admin_id`)
+  `room_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`person_id`),
+  UNIQUE KEY `admin_id_UNIQUE` (`person_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -42,9 +42,44 @@ CREATE TABLE `people` (
 
 LOCK TABLES `people` WRITE;
 /*!40000 ALTER TABLE `people` DISABLE KEYS */;
-INSERT INTO `people` VALUES ('1','Đoàn Quang Huy','Hải Dương','0395018203','huy182','12345abc','custom','1'),('2','Lê Duy','Hà Nội','0931387323','duy1905','12345abc','custom','1'),('4','admin','Hải Dương','1234567890','admin','12345abc','admin','1'),('5','Nguyễn Văn B','Hà Nội','12390234234','b','12345abc','custom','2');
+INSERT INTO `people` VALUES ('1','Đoàn Quang Huy','Hải Dương','0395018203','huy182','12345abc','custom','1'),('2','Lê Duy','Hà Nội','0931387323','duy1905','12345abc','custom','1'),('4','admin','Hải Dương','1234567890','admin','12345abc','admin','0'),('5','Nguyễn Văn B','Hà Nội','12390234234','b','12345abc','custom','2');
 /*!40000 ALTER TABLE `people` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room` (
+  `room_id` varchar(45) NOT NULL,
+  `start_date` varchar(45) NOT NULL,
+  `end_date` varchar(45) NOT NULL,
+  `e_price` double DEFAULT NULL,
+  `w_price` double DEFAULT NULL,
+  `s_price` double DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `r_price` double DEFAULT NULL,
+  PRIMARY KEY (`room_id`),
+  UNIQUE KEY `rom_id_UNIQUE` (`room_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES ('1','18/06/2022','18/09/2022',3.5,28,200,'used',1500),('2','03/03/2022','03/06/2022',3.5,26,150,'used',1900),('3','04/03/2021','04/06/2021',1.5,10,100,'trong',9000);
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'motel_managment'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -55,4 +90,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-25 22:06:30
+-- Dump completed on 2022-09-30 12:15:46
